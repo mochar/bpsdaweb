95de15df9744191554cd2be0bdee231cfbf1aac7
Mo Char <mocharrout@gmail.com>
2015-12-02 19:59:18 +0100

    Stuur alleen bins zonder .binHidden

--- app.py
+++ app.py
@@ -173,10 +173,10 @@ class SijaxHandler(object):
         app.logger.debug(bin_sets)
         binset1 = Binset.query.filter_by(userid=session['uid'],
                                          name=bin_sets[0][0]).first()
-        bins1 = binset1.bins.all()
+        bins1 = [bin for bin in binset1.bins.all() if bin.name in bin_sets[0][1]]
         binset2 = Binset.query.filter_by(userid=session['uid'],
                                          name=bin_sets[1][0]).first()
-        bins2 = binset2.bins.all()
+        bins2 = [bin for bin in binset2.bins.all() if bin.name in bin_sets[1][1]]
         matrix = utils.to_matrix(bins1, bins2)
         # TODO: store color in database someplace else
         colors = ['#FFDD89' for _ in binset1.bins.all()]
