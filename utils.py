78bf3b66ef97504b7cbbc292357e9d9d751d314d
Mo Char <mocharrout@gmail.com>
2015-12-02 21:32:44 +0100

    to_matrix gefixt, hield geen rekening met contigs van bin niet aanwezig in andere bin

--- utils.py
+++ utils.py
@@ -1,17 +1,19 @@
 def to_matrix(bins1, bins2):
     matrix = []
-    for bin1 in bins1:
+    for i, bin1 in enumerate(bins1):
         matching = [0 for _ in bins1]
         for bin2 in bins2:
             matching.append(len([c for c in bin1.contigs
                                  if c in bin2.contigs]))
+        matching[i] = len(bin1.contigs) - sum(matching)
         matrix.append(matching)
-    for bin2 in bins2:
+    for i, bin2 in enumerate(bins2, len(bins1)):
         matching = []
         for bin1 in bins1:
             matching.append(len([c for c in bin2.contigs
                                  if c in bin1.contigs]))
         matching.extend([0 for _ in bins2])
+        matching[i] = len(bin2.contigs) - sum(matching)
         matrix.append(matching)
 
     return matrix
