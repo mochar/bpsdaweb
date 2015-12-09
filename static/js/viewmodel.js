2cd0b5dc4da9e0199d1c17eaf0b6d2f42030ee65
Mo Char <mocharrout@gmail.com>
2015-12-09 16:07:48 +0100

    Chord panel gaat nu via knockout

--- static/js/viewmodel.js
+++ static/js/viewmodel.js
@@ -1,16 +1,37 @@
 function Binset(data) {
     var self = this;
     self.name = ko.observable(data.name);
+    self.color = ko.observable(data.color);
     self.bins = ko.observableArray(data.bins);
+}
+
+function ChordPanel() {
+    var self = this;
+    self.template = "chord";
+    self.firstSelectedBinset = ko.observable();
+    self.secondSelectedBinset = ko.observable();
+
+    self.switchSelectedBinsets = function() {
+        var tmp = self.firstSelectedBinset();
+        self.firstSelectedBinset(self.secondSelectedBinset());
+        self.secondSelectedBinset(tmp);
+    };
 }
 
 function ViewModel() {
     var self = this;
-    self.binsets = ko.observableArray();
+    self.binsets = ko.observableArray([]);
+
+    // Panels
+    self.panels = ko.observableArray([new ChordPanel(), new ChordPanel()]);
+    self.getPanelTemplate = function(panel) {
+        return panel.template;
+    };
 
     $.getJSON('/binsets', function(data) {
         var binsets = $.map(data, function(binset) { return new Binset(binset) });
         self.binsets(binsets);
+        console.log('viewmodel done');
     });
 }
 
