39eeace3830e461c0bacaa4a3de6596c68bfd8eb
Mo Char <mocharrout@gmail.com>
2015-12-09 01:58:01 +0100

    Simpele knockout viewmodel toegevoegd

--- static/js/viewmodel.js
+++ static/js/viewmodel.js
@@ -1,3 +1,19 @@
-/**
- * Created by mochar on 8-12-15.
- */
+function Binset(data) {
+    var self = this;
+    self.name = ko.observable(data.name);
+    self.bins = ko.observableArray(data.bins);
+}
+
+function ViewModel() {
+    var self = this;
+    self.binsets = ko.observableArray();
+
+    $.getJSON('/binsets', function(data) {
+        var binsets = $.map(data, function(binset) { return new Binset(binset) });
+        self.binsets(binsets);
+    });
+}
+
+$(function() {
+    ko.applyBindings(new ViewModel());
+});
