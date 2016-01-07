9115a50a28849ea4ce93beb6bf21bb033fd3362b
Mo Char <mocharrout@gmail.com>
2016-01-07 22:20:08 +0100

    Nieuwe 'dirty flag' toegevoegd

--- static/js/viewmodel.js
+++ static/js/viewmodel.js
@@ -33,6 +33,24 @@ ko.extenders.trackChange = function(target, track) {
     return target;
 };
 
+// http://www.knockmeout.net/2011/05/creating-smart-dirty-flag-in-knockoutjs.html
+ko.dirtyFlag = function(root, isInitiallyDirty) {
+    var result = function() {},
+        _initialState = ko.observable(ko.toJSON(root)),
+        _isInitiallyDirty = ko.observable(isInitiallyDirty);
+
+    result.isDirty = ko.computed(function() {
+        return _isInitiallyDirty() || _initialState() !== ko.toJSON(root);
+    });
+
+    result.reset = function() {
+        _initialState(ko.toJSON(root));
+        _isInitiallyDirty(false);
+    };
+
+    return result;
+};
+
 function BinsetPanel(binset) {
     var self = this;
     self.template = "binsetPanel";
