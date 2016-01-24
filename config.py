02576aa1169d639e778a73a12cbc9896da8e17fa
Mo Char <mocharrout@gmail.com>
2016-01-24 22:19:02 +0100

    Moved debug flag

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ SECRET_KEY = '\x13\xae6\xc7\xc4\xb1\xbf\x164\xe7H\xf5\x7fw9\xd7X\xe0\xb5\x90\xc5
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = False
+
+DEBUG = True
