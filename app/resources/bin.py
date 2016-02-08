1f07ae6ae7421242e3ea9fe411ee7d948c2abf60
Mo Char <mocharrout@gmail.com>
2016-02-08 18:20:47 +0100

    Updated Bin PUT API

--- app/resources/bin.py
+++ app/resources/bin.py
@@ -1,7 +1,8 @@
 from flask.ext.restful import Resource, reqparse
 
 from .utils import bin_or_404
-from app import db
+from app import db, app
+from app.models import Contig
 
 
 class BinApi(Resource):
@@ -9,6 +10,9 @@ class BinApi(Resource):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('name', type=str, location='form')
         self.reqparse.add_argument('color', type=str, location='form')
+        self.reqparse.add_argument('contigs', type=str, location='form')
+        self.reqparse.add_argument('action', type=str, location='form',
+                                   choices=['add', 'remove'])
 
     def get(self, contigset_id, binset_id, id):
         bin = bin_or_404(contigset_id, binset_id, id)
@@ -20,6 +24,21 @@ class BinApi(Resource):
     def put(self, contigset_id, binset_id, id):
         args = self.reqparse.parse_args()
         bin = bin_or_404(contigset_id, binset_id, id)
+
+        if args.contigs:
+            contig_ids = [int(id) for id in args.contigs.split(',')]
+            if args.action == 'add':
+                contigs = bin.binset.contigset.contigs. \
+                    filter(Contig.id.in_(contig_ids)). \
+                    all()
+                bin.contigs.extend(contigs)
+            elif args.action == 'remove':
+                bin.contigs = [c for c in bin.contigs if c.id not in contig_ids]
+            else:
+                contigs = bin.binset.contigset.contigs. \
+                    filter(Contig.id.in_(contig_ids)). \
+                    all()
+                bin.contigs = contigs
         if args.name is not None:
             bin.name = args.name
         db.session.commit()
